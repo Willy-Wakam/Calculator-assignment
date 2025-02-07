674e857b7cc999b5e2ae1aa3c9fd110529b9770b
Willy Wakam <williamswakam09@gmail.com>
2025-02-07 22:25:45 +0100

    Solved some bugs

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const DISPLAY = document.querySelector('.calculator__display');
 
 const BTN_EQUAL = CONTAINER.querySelector('#equal');
 const DISPLAY__RESULT = DISPLAY.querySelector('.display__result');
+const DISPLAY__OPERATIONS = DISPLAY.querySelector('.display__operations');
 const BTN__NODELIST = CONTAINER.querySelectorAll('.btn');
 const OPERATORS = CONTAINER.querySelectorAll('.key--operator');
 const BTN__DELETE = CONTAINER.querySelector('#delete');
@@ -18,30 +19,39 @@ let pressedEqual = false;
 
     
 BTN__DELETE.addEventListener('click', function() {
-    if(DISPLAY__RESULT.textContent.length > 0){
-        const SAVE = DISPLAY__RESULT.textContent.slice(0, -1);
-        DISPLAY__RESULT.textContent = SAVE;
-        if(operator === '') firstOperand = SAVE;
-        else secondOperand = SAVE;
-        if(DISPLAY__RESULT.textContent.includes('.')){
-            BTN__DECIMAL.disabled = true;
+    if(DISPLAY__OPERATIONS.textContent.length > 0){
+        let lastString = DISPLAY__OPERATIONS.textContent.slice(-1);
+        const SAVE = DISPLAY__OPERATIONS.textContent.slice(0, -1);
+        if(lastString === '+' || lastString === '-' || lastString === '×' || lastString === '÷'){
+            operator = '';
         }
-        else {
-            BTN__DECIMAL.disabled = false;
-        }  
+        if(SAVE.endsWith('+') || SAVE.endsWith('-') || SAVE.endsWith('×') || SAVE.endsWith('÷')){
+            secondOperand = '';
+        }
+        if(operator === '') firstOperand = SAVE;
+        else secondOperand = SAVE.slice(firstOperand.length + 1);
+        DISPLAY__OPERATIONS.textContent = SAVE;
+        
+        if(firstOperand.includes('.') && secondOperand === '' || firstOperand.includes('.') && secondOperand.includes('.')) 
+            BTN__DECIMAL.disabled = true;
+        else BTN__DECIMAL.disabled = false;  
     }
-    if (DISPLAY__RESULT.textContent.length === 0){
-        DISPLAY__RESULT.textContent = '0';
+    if (DISPLAY__OPERATIONS.textContent.length === 0){
+        DISPLAY__OPERATIONS.textContent = '0';
     }
+
+    console.log(firstOperand, secondOperand, operator)
 });
 
 BTN__CLEAR.addEventListener('click', function() {
     DISPLAY__RESULT.textContent = '0';
+    DISPLAY__OPERATIONS.textContent = '0';  
+    BTN__DECIMAL.disabled = false; 
     firstOperand = '';
     secondOperand = '';
     operator = '';
-    result = '';
 });
+
 
 // 1st alternative
 /* CONTAINER.addEventListener('click', function (e) {
@@ -127,52 +137,74 @@ const DISPLAY__OPERATIONS = DISPLAY.querySelector('.display__operations');
 function operate (firstOperand, secondOperand, operator){
     switch(operator){
         case '+':
-            return eval(firstOperand + secondOperand);
+            const RESULT_ADD = `${eval(firstOperand + secondOperand)}`;
+            if(RESULT_ADD.length > 10) {
+                if(+RESULT_ADD < 0) return Math.floor(+RESULT_ADD);
+                else return Number((+RESULT_ADD).toFixed(10));
+            };
+            return RESULT_ADD;
         case '-':
-            return eval(firstOperand - secondOperand);
-        case '×':
-            return eval(firstOperand * secondOperand);
-        case '÷':
-            if(secondOperand === 0) return 'Error';
-            const RESULT = +eval(firstOperand / secondOperand).toFixed(9);
-            if(RESULT.length > 9) return RESULT;
+            const RESULT_SUB = `${eval(firstOperand - secondOperand)}`;
+            if(RESULT_SUB.length > 10) {
+                if(+RESULT_SUB < 0) return Math.floor(+RESULT_SUB);
+                else return Number((+RESULT_SUB).toFixed(10));
+            };
+            return RESULT_SUB;
+        case '×' || '*':
+            const RESULT_MUL = `${eval(firstOperand * secondOperand)}`;
+            if(RESULT_MUL.length > 10) return Number((+RESULT_MUL).toFixed(10));
+            return RESULT_MUL;
+        case '÷' || '/':
+            const RESULT = `${eval(firstOperand / secondOperand)}`;
+            if(RESULT.length > 10) return Number((+RESULT).toFixed(10));
             return RESULT;
-        default:
-            return eval(firstOperand * secondOperand);
     }   
 }
 
 CONTAINER.addEventListener('click', function (e) {
-
     for(let i = 0; i < BTN__NODELIST.length; i++) {
         if(e.target === BTN__NODELIST[i]) {
-            if(firstOperand.startsWith('0')){
+            if(firstOperand.startsWith('0') && firstOperand.length === 1){
+                console.log('here');
                 firstOperand = BTN__NODELIST[i].textContent;
                 console.log(firstOperand);
-                DISPLAY__RESULT.textContent = firstOperand
+                console.log(DISPLAY__OPERATIONS.textContent);
+                DISPLAY__OPERATIONS.textContent = firstOperand
+                console.log(DISPLAY__OPERATIONS.textContent);
             }
-            if(pressedEqual) {
+            if(pressedEqual && firstOperand.length < 10) {
+                console.log('here');
                 firstOperand = BTN__NODELIST[i].textContent;
-                DISPLAY__RESULT.textContent = firstOperand;
+                DISPLAY__OPERATIONS.textContent = firstOperand;
                 pressedEqual = false;
             }
-            else if(operator === ''){ 
-                firstOperand += BTN__NODELIST[i].textContent;
-                DISPLAY__RESULT.textContent = firstOperand;
+            else if(operator === '' && firstOperand.length < 10){ 
+                console.log('here');
+                console.log(firstOperand);
+                console.log(DISPLAY__OPERATIONS.textContent);
+                if(BTN__NODELIST[i].textContent === '.' && firstOperand.length === 0) firstOperand = '0.'
+                else firstOperand += BTN__NODELIST[i].textContent;
+                console.log(firstOperand);
+                DISPLAY__OPERATIONS.textContent = firstOperand;
+                console.log(DISPLAY__OPERATIONS.textContent);
             }
-            if(operator !== ''){
+            if(operator !== '' && secondOperand.length < 10){
+                console.log('here');
                 if(secondOperand.startsWith('0')){
                     secondOperand = BTN__NODELIST[i].textContent;
-                    DISPLAY__RESULT.textContent = secondOperand;
+                    DISPLAY__OPERATIONS.textContent += BTN__NODELIST[i].textContent;
+                    console.log(DISPLAY__OPERATIONS.textContent);
                 }
                else {
                 secondOperand += BTN__NODELIST[i].textContent;
-                DISPLAY__RESULT.textContent = secondOperand;
+                console.log(secondOperand);
+                DISPLAY__OPERATIONS.textContent += BTN__NODELIST[i].textContent;
+                console.log(DISPLAY__OPERATIONS.textContent);
                }
             }
-            if(firstOperand.includes('.') && secondOperand === '' || firstOperand.includes('.') && secondOperand.includes('.')) 
-                BTN__DECIMAL.disabled = true;
+            if(firstOperand.includes('.') && secondOperand === '' || (firstOperand.includes('.') && secondOperand.includes('.')))  BTN__DECIMAL.disabled = true;
             else BTN__DECIMAL.disabled = false; 
+            console.log(firstOperand.length)
         }
     }
 
@@ -188,14 +220,21 @@ CONTAINER.addEventListener('click', function (e) {
                     if(operator === '' && secondOperand === ''){
                         pressedEqual = false;
                         operator = OPERATORS[i].textContent;
+                        DISPLAY__OPERATIONS.textContent += operator;
                     }
+                    else if(operator !== '' && secondOperand === ''){
+                        pressedEqual = false;
+                        operator = OPERATORS[i].textContent;
+                        const CHANGE__OPERATOR = DISPLAY__OPERATIONS.textContent.slice(0, -1);
+                        DISPLAY__OPERATIONS.textContent = CHANGE__OPERATOR + operator; 
+                    }   
                     else{
                         pressedEqual = false;
                         DISPLAY__RESULT.textContent = `${operate(+firstOperand, +secondOperand, operator)}`;
                         firstOperand = `${operate(+firstOperand, +secondOperand, operator)}`;
                         secondOperand = '';
-                        operator = '';
                         operator = OPERATORS[i].textContent;
+                        DISPLAY__OPERATIONS.textContent += operator; 
                     }
                 }
                 else if(OPERATORS[i].textContent === '='){
